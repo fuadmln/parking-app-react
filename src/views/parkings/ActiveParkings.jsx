d66699c4db0e17ed11bf6989614f3b7134b48727
Fuad Maulana <fuadm03@gmail.com>
2023-11-05 00:03:42 +0700

    feat: show active parking list

--- src/views/parkings/ActiveParkings.jsx
+++ src/views/parkings/ActiveParkings.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react'
+
 import { Link } from 'react-router-dom'
 import { route } from '@/routes'
+import axios from 'axios'
 
 function ActiveParkings(){
+  const [parkings, setParkings] = useState([])
+
+  useEffect(() => {
+    const controller = new AbortController()
+    getActiveParkings({ signal: controller.signal })
+    return () => controller.abort()
+  }, [])
+
+  async function getActiveParkings({ signal  = {} }){
+    return axios.get('parkings', { signal })
+      .then(response => setParkings(response.data.data))
+      .catch(() => {})
+  }
+
+  async function stopParking(parking){
+    await axios.put(`parkings/${parking.id}`)
+    await getActiveParkings({ signal: new AbortController().signal })
+  }
+
   return (
     <div className="flex flex-col mx-auto md:w-96 w-full">
 
@@ -13,8 +35,38 @@ function ActiveParkings(){
 
       <div className="border-t h-[1px] my-6"></div>
 
-      <div>
-        There will be active parkings list
+      <div className="flex flex-col gap-1">
+        { parkings.length > 0 && parkings.map(parking => (
+          <div key={ parking.id } className="flex flex-col p-2 border gap-1">
+            <div className="plate text-2xl">
+              { parking.vehicle.plate_number }
+            </div>
+            <div className="text-sm text-gray-600">
+              { parking.vehicle.description }
+            </div>
+            <div className="bg-gray-100 p-2">
+              { parking.zone.name }
+              { `Rp. ${parking.zone.price_per_hour}/h` }
+            </div>
+            <div>
+              <div className="font-bold uppercase">from</div>
+              <span className="font-mono">{ parking.start_time }</span>
+            </div>
+            <div className="flex items-top">
+              <span className="pt-0.5">Rp. </span>
+              <span className="text-2xl font-bold text-blue-600">
+                { parking.total_price }
+              </span>
+            </div>
+            <button
+              type="button"
+              className="btn btn-danger uppercase ml-auto"
+              onClick={ () => stopParking(parking) }
+            >
+              stop
+            </button>
+          </div>
+        )) }
       </div>
     </div>
   )
